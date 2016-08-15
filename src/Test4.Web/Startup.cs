9300ad942132db4e7fcc27712fc5b22b7b2f825f
devtriad <devtriad@gmail.com>
2016-08-15 20:39:54 +0200

    Test4 Sub Application in IIS

--- src/Test4.Web/Startup.cs
+++ src/Test4.Web/Startup.cs
@@ -12,15 +12,21 @@ using Microsoft.Extensions.Logging;
 using Test4.Web.Data;
 using Test4.Web.Models;
 using Test4.Web.Services;
+using System.IO;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Http;
 
 namespace Test4.Web
 {
     public class Startup
     {
+
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
+                  .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
@@ -39,24 +45,67 @@ namespace Test4.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddMvc();
+            services.Configure<RouteOptions>(
+                options => {
+                    options.AppendTrailingSlash = false;
+                    options.LowercaseUrls = true;
+                }
+                );
+
+
+
+
+            // Session variables support
+            //services.AddCaching();
+            services.AddSession();
+
+            //upgrade to asp.net core - use propercase and not camelcase for json properties
+            services.AddMvc()
+              .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
 
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
+
+            ////change default login location from /Account/Login to /Area/Account/Login
+            services.Configure<CookieAuthenticationOptions>(options =>
+            {
+                options.LoginPath = new PathString("/Account/Login");
+            });
+
+
+            services.Configure<IISOptions>(options => { });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            //another hack
+            //https://github.com/aspnet/Hosting/issues/416
+
+            if (env.IsDevelopment())
+            {
+                this.Configure1( app, env, loggerFactory);
+            }
+            else
+            {
+                //this.Configure1(app, env, loggerFactory);
+                app.Map("/Test4", (app1) => this.Configure1(app1, env, loggerFactory));
+            }
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure1(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+     
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
@@ -64,25 +113,31 @@ namespace Test4.Web
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
-                app.UseBrowserLink();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+
+    
             }
 
-            app.UseStaticFiles();
 
             app.UseIdentity();
 
-            // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
+
+            app.UseSession();
+
+            app.UseStaticFiles();
 
             app.UseMvc(routes =>
             {
+
+                routes.MapRoute("areaRoute", "{area:exists}/{controller}/{action}");
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
     }
 }
